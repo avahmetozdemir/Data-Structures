693c7f5b36815df1845294153ef785e2475a8847
avahmetozdemir <avamertozdemir@gmail.com>
2022-07-23 12:58:22 +0300

    09- add binarysearchtree-traversal&height

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -122,22 +122,134 @@ class BST {
         }
         this.root= removeNode(this.node,data)
     }
+    isBalanced() {
+        return (this.findMinHeight() >= this.findMaxHeight() - 1)
+      }
+      findMinHeight(node = this.root) {
+          if (node == null) {
+              return -1;
+          };
+          let left = this.findMinHeight(node.left);
+          let right = this.findMinHeight(node.right);
+          if (left < right) {
+              return left + 1;
+          } else {
+              return right + 1;
+          };
+      }
+      findMaxHeight(node = this.root) {
+          if (node == null) {
+              return -1;
+          };
+          let left = this.findMaxHeight(node.left);
+          let right = this.findMaxHeight(node.right);
+          if (left > right) {
+              return left + 1;
+          } else {
+              return right + 1;
+          };
+      }
+      inOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traverseInOrder(node) {       
+            node.left && traverseInOrder(node.left);
+            result.push(node.data);
+            node.right && traverseInOrder(node.right);
+          }
+          traverseInOrder(this.root);
+          return result;
+        };
+      }
+      preOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traversePreOrder(node) {
+            result.push(node.data);
+            node.left && traversePreOrder(node.left);
+            node.right && traversePreOrder(node.right);
+          };
+          traversePreOrder(this.root);
+          return result;
+        };
+      }
+      postOrder() {
+        if (this.root == null) {
+          return null;
+        } else {
+          var result = new Array();
+          function traversePostOrder(node) {
+            node.left && traversePostOrder(node.left);
+            node.right && traversePostOrder(node.right);
+            result.push(node.data);
+          };
+          traversePostOrder(this.root);
+          return result;
+        }
+      }
+      
+      levelOrder() {
+          let result = [];
+          let Q = []; 
+          if (this.root != null) {
+              Q.push(this.root);
+              while(Q.length > 0) {
+                  let node = Q.shift();
+                  result.push(node.data);
+                  if (node.left != null) {
+                      Q.push(node.left);
+                  };
+                  if (node.right != null) {
+                      Q.push(node.right);
+                  };
+              };
+              return result;
+          } else {
+              return null;
+          };
+      };
 }
 
 const bst =  new BST()
 
-bst.add(4)
-bst.add(2)
-bst.add(6)
-bst.add(1)
-bst.add(3)
-bst.add(5)
-bst.add(7)
-bst.remove(4)
-console.log(bst.findMin());
-console.log(bst.findMax());
-bst.remove(7)
-console.log(bst.findMax());
-console.log(bst.isPresent(4));
+// bst.add(4)
+// bst.add(2)
+// bst.add(6)
+// bst.add(1)
+// bst.add(3)
+// bst.add(5)
+// bst.add(7)
+// bst.remove(4)
+// console.log(bst.findMin());
+// console.log(bst.findMax());
+// bst.remove(7)
+// console.log(bst.findMax());
+// console.log(bst.isPresent(4));
 
 
+bst.add(9);
+bst.add(4);
+bst.add(17);
+bst.add(3);
+bst.add(6);
+bst.add(22);
+bst.add(5);
+bst.add(7);
+bst.add(20);
+
+console.log(bst.findMinHeight());
+console.log(bst.findMaxHeight());
+console.log(bst.isBalanced());
+bst.add(10);
+console.log(bst.findMinHeight());
+console.log(bst.findMaxHeight());
+console.log(bst.isBalanced());
+console.log('inOrder: ' + bst.inOrder());
+console.log('preOrder: ' + bst.preOrder());
+console.log('postOrder: ' + bst.postOrder());
+
+console.log('levelOrder: ' + bst.levelOrder());
